df755bf627dc6ae92af856508296a0cc9c71d015
bshongwe <dru.da.beat.hustle@gmail.com>
2023-09-06 07:44:13 -0700

    Task 4 - atou.c

---
+++ atou.c
@@ -0,0 +1,30 @@
+#include "builtins.h"
+
+/**
+ * atou - func converts string to int
+ * @s: char int to verify and convert
+ * Return: conveted int (Success)
+ */
+unsigned int atou(char *s)
+{
+	size_t a;
+	unsigned int num = 0;
+	unsigned int to_add;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		if (UINT_MAX / 10 < NUM)
+		{
+			return (UNIT_MAX);
+		}
+
+		num *= 10;
+		to_add = s[a] - '0';
+		if (UINT_MAX - to_add < num)
+		{
+			return (UINT_MAX);
+		}
+		num += to_add;
+	}
+	return (num);
+}
