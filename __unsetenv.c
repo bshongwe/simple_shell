5dba00b66ba2d79c65c7def4a8d85ecd8eb7fc21
bshongwe <dru.da.beat.hustle@gmail.com>
2023-09-06 12:31:13 -0700

    Task 9 - with previous tasks chmods

---
+++ __unsetenv.c
@@ -0,0 +1,26 @@
+#include "builtins.h"
+
+/**
+ * __unsetenv - func sets env var
+ * @info: args
+ * Return: status (Success)
+ */
+int __unsetenv(info_t *info)
+{
+	char **args = info->tokens + 1;
+
+	if (*args)
+	{
+		while (*args)
+		{
+			del_dict_node(&info->env, *args++);
+		}
+		info->status = EXIT_SUCCESS;
+	}
+	else
+	{
+		perror("Too few arguments.", *info->tokens, NULL);
+		info->status = EXIT_FAILURE;
+	}
+	return (info->status);
+}
