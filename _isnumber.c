b6c046729d2f2ce723ec6ef7ba2be78bca8f53bb
bshongwe <dru.da.beat.hustle@gmail.com>
2023-09-06 16:47:31 -0700

    used in __exit.c

---
+++ _isnumber.c
@@ -0,0 +1,22 @@
+#include /* header */
+
+/**
+ * _isnumber - func verifies string for strictly digits
+ * @s: input string pointer
+ * Return: true or false (boolean)
+ */
+bool _isnumber(const char *s)
+{
+	if (s)
+	{
+		while (*s)
+		{
+			if (!_isdigit(*s++))
+			{
+				return (false);
+			}
+		}
+		return (true);
+	}
+	return (false);
+}
