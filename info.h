9c5235b5cb822b78ce835c6e3e0cc6d1a450c4ca
bshongwe <dru.da.beat.hustle@gmail.com>
2023-09-09 08:56:49 -0700

    add-ins

--- info.h
+++ info.h
@@ -2,6 +2,7 @@
 #define INFO_H
 
 #include <unistd.h>
+#include <stdlib.h>
 #include "env.h"
 #include "getline.h"
 #include "string.h"
@@ -9,28 +10,48 @@
 #include "command.h"
 #include "alias.h"
 #include "error.h"
+#include "tokens.h"
 
 extern char **environ;
 
 /**
  * struct info - shell state
  * @fileno: args passed
+ * @file: args passed
  * @line: args passed
  * @env: args passed
  * @path: args passed
  * @aliases: args passed
  * @commands: args passed
+ * @line: args passed
+ * @lineno: args passed
+ * @tokens: args passed
+ * @cwd: args passed
+ * @argc: args passed
+ * @argv: args passed
+ * @exe: args passed
+ * @status: args passed
  */
 struct info
 {
 	char *file;
 	int *fileno;
+	int argc;
+	int status;
 	char *line;
+	char *cwd;
+	char **argv;
 	size_t lineno;
-	charr **tokens;
+	char *exe
+	char **tokens;
 	env_t *env;
+	list_t *path;
+	alias_t *aliases;
 	cmdlist_t *commands;
 };
+
+int free_info(info_t *info);
+info_t *intit_info(int argc, char **argv);
 
 
 #endif /* end of INFO_H file */
--- path.h
+++ path.h
@@ -5,7 +5,9 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <stdlib.h>
-#include <string.h>
+#include "string.h"
+#include "info.h"
+#include "types.h"
 
 char *search_path(info_t *info, list_t *path);
 
