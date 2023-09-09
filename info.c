da6420cc970ea0e1c3f5d620ed0caa7a4cad6a14
bshongwe <dru.da.beat.hustle@gmail.com>
2023-09-09 07:09:57 -0700

    Does this answer part of the pid Qs you had?

---
+++ info.c
@@ -0,0 +1,59 @@
+#include "info.h"
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+/**
+ * free_info - func frees and nulls dynam-alloc info
+ * @info: info pointer
+ * Return: exit status (Success)
+ */
+int free_info(info_t *info)
+{
+	free(info->line);
+	info->line = _getline(-1);
+	free_tokens(&info->tokens);
+	free(info->cwd);
+	info->cwd = NULL;
+	free(info->exe);
+	info->exe = NULL;
+	free_dict(&info->env);
+	free_list(&info->path);
+	free_dict(&info->aliases);
+	free_cmdlist(&info->commands);
+	return (info->status);
+}
+
+/**
+ * init_info - func invokes shell info
+ * @argc: arg count
+ * @argv: arg values
+ * Return: info pointer
+ */
+info_t *init_info(int argc, char **argv)
+{
+	char *error = NULL;
+	static info_t info;
+
+	info.argc = argc;
+	info.argv = argv;
+	info.fileno = STDIN_FILENO;
+	if (argc > 1)
+	{
+		info.file = argv[1];
+		info.fileno = open(info.file, O_RDONLY);
+		if (info.fileno == -1)
+		{
+			error = strjoin(NULL, " ", "Can not open", info.file);
+			perrorl_default(*argv, info.lineno, error, NULL);
+			free(error);
+			info.status = 127;
+			exit(free_info(&info));
+		}
+	}
+	info.interactive = isatty(info.fileno);
+	info.pid = getpid();
+	info.cwd = getcwd(NULL, 0);
+	info.env = env_to_dict(environ);
+	return (&info);
+}
