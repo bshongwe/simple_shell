385b05cadf6eefe36c32337a55a34e393c4f4428
bshongwe <dru.da.beat.hustle@gmail.com>
2023-09-08 22:38:20 -0700

    alias headers + typedef struct headers

---
+++ alias.h
@@ -0,0 +1,9 @@
+#ifndef ALIAS_H
+#define ALIAS_H
+
+#include "types.h"
+#include "dict.h"
+
+typedef dict_t alias_t;
+
+#endif /* end of ALIAS_H file */
---
+++ types.h
@@ -0,0 +1,9 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef struct dict dict_t;
+typedef struct cmdlist cmdlist_t;
+typedef struct cmdtree cmdtree_t;
+typedef struct builtin builtin_t;
+
+#endif /* end of TYPES_H */
