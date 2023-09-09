d6a92e0d8b65d889dd35e58d5056be1a9d991864
Garrick Larson Potts <ricki.glp@gmail.com>
2023-09-09 06:17:51 -0700

    completed _isident.c was not sure if you wanted it to check only a char or a whole string but I went with char

--- _isident.c
+++ _isident.c
@@ -7,5 +7,17 @@
  */
 bool _isident(int c)
 {
-	/* complete this */
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_')
+	{
+		return (1);
+	}
+	else if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
 }
