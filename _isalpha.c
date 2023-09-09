d0ed69151c5630e8d5b8d5f2d54de3293ed3686d
bshongwe <dru.da.beat.hustle@gmail.com>
2023-09-08 23:59:33 -0700

    complete the logic

---
+++ _isalpha.c
@@ -0,0 +1,11 @@
+#include "ctype.h"
+
+/**
+ * _sialpha - func verifies if char is alphabetic
+ * @c: char input
+ * Return: c is alphabetic (1), is not alphabetic (0)
+ */
+bool _isalpha(int c)
+{
+	/* complete this */
+}
--- ctype.h
+++ ctype.h
@@ -5,5 +5,11 @@
 #include <stdbool.h>
 
 bool _isnumber(const char *s);
+bool _isalnum(int c);
+bool _isdigit(int c);
+bool _isident(int c);
+bool _isquote(int c);
+bool _isspace(int c);
+bool _isalpha(int c);
 
 #endif /* end of CTYPE_H file */
