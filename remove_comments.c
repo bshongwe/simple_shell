d8e6f3c66417215b3d0268ddf280e795e46964f6
Garrick Larson Potts <ricki.glp@gmail.com>
2023-09-10 04:49:22 -0700

    added the remove_contents.c file

---
+++ remove_comments.c
@@ -0,0 +1,27 @@
+#include "hsh.h"
+
+/**
+ * remove_comments - Removes comments (#) from a command.
+ * @cmd: Pointer to ther cmd process.
+ * Return: Nothing.
+ */
+
+void remove_comments(cmdlisr_t *cmd)
+{
+	char **tokens, **new, *tmp;
+
+	for (tokens = cmd->tokens; *tokens; ++tokens)
+	{
+		if (**tokens == '#')
+		{
+			tmp = *tokens;
+			*tokens = NULL;
+			new = arrdup(cmd->tokens);
+			*tokens = tmp;
+			free_tokens(&(cmd->tokens));
+			cmd->tokens = new;
+			free_cmdlist(&(cmd->next));
+			return;
+		}
+	}
+}
