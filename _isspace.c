daedfd7a53c8887a28e79916027c09af53b7fcc3
bshongwe <dru.da.beat.hustle@gmail.com>
2023-09-11 02:52:24 -0700

    _isspace.c edit

--- _isspace.c
+++ _isspace.c
@@ -8,24 +8,4 @@
 bool _isspace(int c)
 {
 	return (c == ' ' || (c >= 0x09 && c <= 0x0d));
-}#include "ctype.h"
-
-/**
- * _isspace - func verifies if char is whitespace
- * @c: char input
- * Return: char is whitespace (1), otherwise (0)
- */
-bool _isspace(int c)
-{
-	return (c == ' ' || (c >= 0x09 && c <= 0x0d));
-}#include "ctype.h"
-
-/**
- * _isspace - func verifies if char is whitespace
- * @c: char input
- * Return: char is whitespace (1), otherwise (0)
- */
-bool _isspace(int c)
-{
-	return (c == ' ' || (c >= 0x09 && c <= 0x0d));
 }
